b606bae8e21666ca35e1f450820ac4a29012f648
WAYU SAENGPHITHAK <68947889+WavaXD@users.noreply.github.com>
2022-04-08 20:32:18 +0700

    Add files via upload

---
+++ Lab11_BDP2/SortStrategy.java
@@ -0,0 +1,13 @@
+package lab11;
+
+/**
+ *
+ * @author Asus
+ */
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+}
---
+++ Lab11_BDP2/WeightStrategy.java
@@ -0,0 +1,16 @@
+package lab11;
+
+/**
+ *
+ * @author Asus
+ */
+
+import java.util.Comparator;
+import java.util.List;
+
+public class WeightStrategy implements SortStrategy {
+    @Override
+    public void customSort(List<Person> people){
+        people.sort(Comparator.comparing(Person::getWeight));
+    }
+}
